ff8aea7114e3ce43caf18cff74d19ebb5a1be468
Emmanuel Alves <emmanuel@MacBook-Pro-de-Emmanuel.local>
2015-10-24 22:22:23 -0400

    - solved problem with same origin policy

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -374,6 +374,15 @@ abstract class REST_Controller extends CI_Controller {
     {
         parent::__construct();
 
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
+
         // Disable XML Entity (security vulnerability)
         libxml_disable_entity_loader(TRUE);
 
